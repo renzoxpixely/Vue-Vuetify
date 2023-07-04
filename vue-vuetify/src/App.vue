7cd07296084593d36d072ccb01366313ad0e9c3d
Renzo O <renzo_or@hotmail.com>
2023-07-04 17:32:15 -0500

    add navbar

---
+++ vue-vuetify/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  
+  <v-app>
+
+    <NavbarComponent></NavbarComponent>
+    <v-content>
+      <router-view/>
+    </v-content>
+    
+  </v-app>
+
+</template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld";
+import NavbarComponent from "./components/Navbar";
+
+export default {
+  components: {NavbarComponent},
+  name: "App",
+  data(){
+    return {
+      appTitle: 'Awesome App',
+      sidebar: false,
+      menuItems: [
+          { title: 'Login', path: '/', icon: 'home' },
+          { title: 'About', path: '/about', icon: 'face' },
+          { title: 'Home', path: '/home', icon: 'lock_open' }
+     ]
+    }
+  },
+};
+</script>
+<style>
+
+</style>
